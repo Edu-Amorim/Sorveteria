8023dfe662d04eb811f5af6feb877588d37fff93
Edu-Amorim <eduardohmamorim@gmail.com>
2020-12-16 19:41:35 -0300

    parei de usar o sort() e adicionei a funcionalidade de contratar vendedores

--- sorveteria.js
+++ sorveteria.js
@@ -1,40 +1,71 @@
-const vendas = [];
 let somaDosValoresDoDia = 0;
 let quantidadeDeVendasDoDia = 0;
 let valorMedioDosPedidos = (_somaDosValoresDoDia, _quantidadeDeVendasDoDia) => _somaDosValoresDoDia/_quantidadeDeVendasDoDia;
 
-function pedido (vendaDoDia, nome, valor, sabor, data) {
+const vendas = [];
+const meusVendedores = [];
+
+function meuVendedor (vendedor, rg, anoNascimento, dataContratacao, quantoVendeu = 0) {
+    this.vendedor = vendedor;
+    this.rg = rg;
+    this.anoNascimento = anoNascimento;
+    this.dataContratacao = new Date().toUTCString();
+    this.quantoVendeu = quantoVendeu;
+}
+
+let contratarVendedor = (vendedor, rg, anoNascimento) => {
+    meusVendedores.push(new meuVendedor(vendedor, rg, anoNascimento));
+}
+
+contratarVendedor('Lobão', 6125615, 1980)
+contratarVendedor('Dimas', 4242122, 1982)
+contratarVendedor('Massao', 23213123, 1990)
+
+function pedido (vendaDoDia, vendedor, cliente, valor, sabor, data) {
     this.vendaDoDia = vendaDoDia;
-    this.nome = nome;
+    this.vendedor = vendedor;
+    this.cliente = cliente;
     this.valor = valor;
     this.sabor = sabor;
     this.data = new Date().toUTCString();
 }
 
-let vender = (nome, valor, sabor) => {
+let vender = (vendedor, cliente, valor, sabor) => {
     quantidadeDeVendasDoDia++;
-    vendas.push(new pedido(quantidadeDeVendasDoDia, nome, valor, sabor));
+    vendas.push(new pedido(quantidadeDeVendasDoDia, vendedor, cliente, valor, sabor));
     somaDosValoresDoDia += valor;
-    //valorMedioDosPedidos = somaDosValoresDoDia/quantidadeDeVendasDoDia;
+
+    for (let i in meusVendedores) {
+        if (meusVendedores[i].vendedor === vendedor) {
+            meusVendedores[i].quantoVendeu += valor;
+        }
+    }
 }
 
 let relatorio = () => {
-    let vendasOrdenadasPorValor = vendas.sort(function (a, b) {return a.valor - b.valor;});
-    let pedidoMaisCaro = vendasOrdenadasPorValor[vendasOrdenadasPorValor.length -1];
-    let pedidoMaisBarato = vendasOrdenadasPorValor[0];
-    
+    let pedidoMaisCaro = vendas.reduce((prev, current) => (prev.valor > current.valor) ? prev : current);
+    let pedidoMaisBarato = vendas.reduce((prev, current) => (prev.valor < current.valor) ? prev : current);
+    let melhorVendedor = meusVendedores.reduce((prev, current) => (prev.quantoVendeu > current.quantoVendeu) ? prev : current);
     console.log(`Olá, hoje nós vendemos ${quantidadeDeVendasDoDia} sorvetes, somando R$${somaDosValoresDoDia}. O preço médio de cada pedido foi ${valorMedioDosPedidos(somaDosValoresDoDia, quantidadeDeVendasDoDia)}.`);
     console.log("\nPara uma análise um pouco mais detalhada, segue outras informações:");
-    console.log("O pedido mais caro foi: \n");
+    console.log("\nO pedido mais caro foi:");
     console.log(pedidoMaisCaro)
-    console.log("O pedido mais barato foi: \n")
+    console.log("\nO pedido mais barato foi:")
     console.log(pedidoMaisBarato)
+    console.log("\nO vendedor que mais vendeu foi o")
+    console.log(melhorVendedor)
     console.log("\nAqui vão todos os pedidos do dia: \n");
-    console.log(vendas.sort(function (a, b) {return a.vendaDoDia - b.vendaDoDia;}))
+    console.log(vendas)
 }
 
-vender("Eduardo", 4, "chocolate belga");
-vender("Rafael", 10, "banana");
-vender("Ju", 2.50, "morango");
+vender("Lobão", "Eduardo", 4, "chocolate belga");
+vender("Dimas", "Rafael", 10, "banana");
+vender("Massao", "Ju", 2.50, "morango");
+vender("Lobão", "Fabiana", 5, "chocolate meio amargo");
+vender("Dimas", "Lígia", 7, "frutas vermelhas");
+vender("Massao", "Papeleo", 9.50, "morango");
+vender("Lobão", "João", 3.5, "chocolate belga");
+vender("Dimas", "Rafael", 10, "banana");
+vender("Massao", "Bruno", 12.50, "morango");
 
 relatorio();
